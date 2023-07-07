1a4d2f6d080bbaae510f4973be3aaa80db6e80ff
matthewruben <mruben.aggie@gmail.com>
2023-07-07 12:30:33 -0600

    2h moved the code out of MainActivity into is own class

--- app/src/main/kotlin/org/jdc/template/ux/main/MainActivity.kt
+++ app/src/main/kotlin/org/jdc/template/ux/main/MainActivity.kt
@@ -10,19 +10,9 @@ import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.jdc.template.model.data.DisplayThemeType
 import org.jdc.template.model.webservice.directory.DirectoryService
-import org.jdc.template.model.webservice.directory.data.PersonsDta
 import org.jdc.template.ui.theme.AppTheme
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import timber.log.Timber
-import java.lang.Exception
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -57,48 +47,6 @@ class MainActivity : ComponentActivity() {
                 MainScreen()
             }
         }
-        makeAPIRequest() //~~~~~ DO NOT WANT TO HAVE THIS HERE ~~~~~~~~~
-    }
-
-    //~~~~~~~~~~~~~MOVE TO OWN CLASS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    private fun makeAPIRequest() {
-        val api = Retrofit.Builder()
-                .baseUrl(DirectoryService.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(DirectoryService::class.java)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val response = api.directoryByFullURL(DirectoryService.FULL_URL)
-                if (response.isSuccessful)
-                {
-                    processWebServiceResponse(response)
-                }
-                else {
-            Timber.e("API Call FAILED [${response.errorBody()}]")
-                }
-            } catch (e: Exception) {
-                Timber.tag("Main").e("Error: %s", e.message)
-            }
-        }
-
-    }
-
-    fun processWebServiceResponse(response: Response<PersonsDta>)
-    {
-        if (response.isSuccessful) {
-            Timber.tag("Main").d("response: %s", response.isSuccessful)
-            response.body()?.let {
-                processData(it)
-            }
-        }
-    }
-
-    fun processData(personsDta: PersonsDta)
-    {
-        for (dtaResult in personsDta.individuals) {
-            Timber.i("Results: %s", dtaResult.firstName)
-        }
+        DirectoryService().getDirectory()
     }
 }
